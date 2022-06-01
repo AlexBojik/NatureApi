9cda995e49d14ca5b7b52ce97ac2827d23b8d9a7
Derevenskikh Aleksander <hedgehog@kartis.ru>
2022-06-01 15:28:50 +0300

    run locally

---
+++ go.mod
@@ -0,0 +1,3 @@
+module water-api
+
+go 1.18
--- server.go
+++ server.go
@@ -5,7 +5,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-	"net/http"
+	http "net/http"
 	"os"
 	h "water-api/handlers"
 	"water-api/models"
